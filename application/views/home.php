40f1b2e88f25e700acb89185f6a609bd0f81a7f7
Danny ingin setia <julianpratamad@gmail.com>
2021-08-24 10:07:20 +0700

    Cek lagi

---
+++ application/views/home.php
@@ -0,0 +1,5 @@
+<h1>Home</h1>
+
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae vero, modi temporibus id ullam quis. Corrupti provident, laudantium laboriosam fugit, earum totam temporibus beatae eum quos vitae illum impedit inventore.
+</p>
